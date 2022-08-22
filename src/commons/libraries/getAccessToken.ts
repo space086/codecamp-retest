99189a196af4113f790b2a52e26792c8b68e0c9c
space086 <space_086@naver.com>
2022-08-22 23:39:36 +0900

    :수정: graphql 주소 변경

--- src/commons/libraries/getAccessToken.ts
+++ src/commons/libraries/getAccessToken.ts
@@ -1,5 +1,5 @@
-import { gql} from "@apollo/client"
-import { GraphQLClient } from "graphql-request"
+import { gql } from "@apollo/client";
+import { GraphQLClient } from "graphql-request";
 
 const RESTORE_ACCESS_TOKEN = gql`
   mutation restoreAccessToken {
@@ -9,13 +9,12 @@ const RESTORE_ACCESS_TOKEN = gql`
   }
 `;
 
-
 // 엑세스 토큰 재발급해주는
 export async function getAccessToken() {
   try {
     // 2-1.refreshToken으로 accessToken을 재발급 받기
     const graphQLClient = new GraphQLClient(
-      "https://backend07.codebootcamp.co.kr/graphql",
+      "https://backend07.codebootcamp.co.kr/graphql14",
       { credentials: "include" }
     );
     const result = await graphQLClient.request(RESTORE_ACCESS_TOKEN);
--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -1,8 +1,13 @@
-import { ApolloClient,ApolloLink,ApolloProvider,InMemoryCache } from "@apollo/client"
-import { createUploadLink } from "apollo-upload-client"
-import { useRecoilState } from 'recoil'
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  InMemoryCache,
+} from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
+import { useRecoilState } from "recoil";
 import { ReactNode, useEffect } from "react";
-import { onError } from "@apollo/client/link/error"
+import { onError } from "@apollo/client/link/error";
 import { getAccessToken } from "../../../commons/libraries/getAccessToken";
 import { accessTokenState, basketState } from "../../../commons/store";
 import { gql } from "graphql-request";
@@ -11,54 +16,48 @@ interface IProps {
   children: ReactNode;
 }
 
-export default function ApolloSetting(props: IProps){
+export default function ApolloSetting(props: IProps) {
   const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
-  const [,setBasketItems] = useRecoilState(basketState)
+  const [, setBasketItems] = useRecoilState(basketState);
 
-  useEffect(()=>{
+  useEffect(() => {
     getAccessToken().then((newAccessToken) => {
       setAccessToken(newAccessToken);
     });
-    const baskets = JSON.parse(localStorage.getItem("baskets") || "[]")
-    setBasketItems(baskets)
-  },[])
+    const baskets = JSON.parse(localStorage.getItem("baskets") || "[]");
+    setBasketItems(baskets);
+  }, []);
 
-  const errorLink = onError(({graphQLErrors, operation, forward}) => {
-
-    if(graphQLErrors) {
-      for(const err of graphQLErrors) {
-        if(err.extensions.code === 'UNAUTHENTICATED'){
+  const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+    if (graphQLErrors) {
+      for (const err of graphQLErrors) {
+        if (err.extensions.code === "UNAUTHENTICATED") {
           getAccessToken().then((newAccessToken) => {
-            setAccessToken(newAccessToken)
+            setAccessToken(newAccessToken);
             operation.setContext({
-              headers: { 
-              ...operation.getContext().headers,
-              Authorization: `Bearer ${newAccessToken}`,
+              headers: {
+                ...operation.getContext().headers,
+                Authorization: `Bearer ${newAccessToken}`,
               },
             });
-          return forward(operation)
-        })
+            return forward(operation);
+          });
         }
       }
-    } 
+    }
   });
 
   const uploadLink = createUploadLink({
-    uri: "https://backend07.codebootcamp.co.kr/graphql",
-    headers: {Authorization: `Bearer ${accessToken}`},
+    uri: "https://backend07.codebootcamp.co.kr/graphql14",
+    headers: { Authorization: `Bearer ${accessToken}` },
     credentials: "include",
-  }) 
+  });
 
   const client = new ApolloClient({
     link: ApolloLink.from([errorLink, uploadLink as unknown as ApolloLink]),
-    cache: new InMemoryCache(), 
+    cache: new InMemoryCache(),
     connectToDevTools: true,
-  })
+  });
 
-
-  return(
-    <ApolloProvider client={client}>
-      {props.children}
-    </ApolloProvider>
-  )
+  return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
 }
